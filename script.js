ae13016c6dc47fc20e2749fd6ea6fc5defca203b
Sahil pillania <sahilpillania88@gmail.com>
2023-03-08 12:15:57 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,97 @@
+const hrs = document.getElementById("hrs");
+const min = document.getElementById("min");
+const sec = document.getElementById("sec");
+const msec = document.getElementById("msec");
+const start = document.getElementById("start");
+const stop = document.getElementById("stop");
+const reset = document.getElementById("reset");
+
+// msec.innerHTML =
+var Interval;
+start.addEventListener("click", () => {
+  Interval = setInterval(() => {
+    msec.innerHTML++;
+
+    if (msec.innerHTML <= 9) {
+      msec.innerHTML = "0" + msec.innerHTML;
+    }
+
+    if (msec.innerHTML == 100) {
+      msec.innerHTML = 0;
+      //   sec.innerHTML = parseInt(sec.innerHTML) + 1;
+      secFunc();
+    }
+  }, 10);
+});
+
+// handling secs
+var secFunc = () => {
+  // sec.innerHTML = parseInt(sec.innerHTML) + 1;
+  sec.innerHTML++;
+  if (sec.innerHTML <= 9) {
+    sec.innerHTML = "0" + sec.innerHTML;
+  }
+
+  if (sec.innerHTML == 60) {
+    sec.innerHTML = 0;
+    // min.innerHTML = parseInt(min.innerHTML) + 1;
+    minFunc();
+  } else {
+  }
+};
+
+// handling mins
+var minFunc = () => {
+  // sec.innerHTML = parseInt(sec.innerHTML) + 1;
+  if (min.innerHTML <= 9) {
+    min.innerHTML = "0" + min.innerHTML;
+  }
+
+  if (min.innerHTML >= 60) {
+    min.innerHTML = 0;
+    // hrs.innerHTML = parseInt(hrs.innerHTML) + 1;
+    hrsFunc();
+  } else {
+    min.innerHTML++;
+  }
+};
+// handling hours
+var hrsFunc = () => {
+  // sec.innerHTML = parseInt(sec.innerHTML) + 1;
+  if (hrs.innerHTML <= 9) {
+    hrs.innerHTML = "0" + hrs.innerHTML;
+  }
+
+  if (hrs.innerHTML >= 60) {
+    hrs.innerHTML = 0;
+    // hrs.innerHTML = parseInt(hrs.innerHTML) + 1;
+    stopFunc();
+    alert("Timer is fully filled");
+  } else {
+    hrs.innerHTML++;
+  }
+};
+// stop function expression
+var stopFunc = () => {
+  clearInterval(Interval);
+  console.log("stopping");
+};
+
+// reset function expression
+var resetFunc = () => {
+  clearInterval(Interval);
+  msec.innerHTML = "00";
+  sec.innerHTML = " 00";
+  min.innerHTML = "00";
+  hrs.innerHTML = "00";
+};
+
+// callinbg stop function
+
+stop.addEventListener("click", () => {
+  stopFunc();
+});
+// calling reset function
+reset.addEventListener("click", () => {
+  resetFunc();
+});
